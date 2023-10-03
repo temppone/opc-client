2545610305d6ca5a3aedab7bf87069bd47069819
Gustavo Tempone de Oliveira Faria <gustavo.faria@efrete.com.br>
2023-10-03 07:39:03 -0300

    feat: build output config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    outDir: "build",
+  },
+
   test: {
     globals: true,
     environment: "jsdom",
